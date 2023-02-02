036ba8611dbe2f0bfe49949ef82cd38b4ec84160
ameenalasady <105679326+ameenalasady@users.noreply.github.com>
2023-02-02 18:21:58 -0500

    Update Lab1Part2.c

--- Lab1Part2.c
+++ Lab1Part2.c
@@ -6,20 +6,21 @@
 
 int main()
 {
-    volatile char hex_code[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
+    volatile int hex_code[16] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
     int offOrOn = 1;
 
     while (1)
     {
         if (offOrOn == 1)
         {
-            char switchValue = (*(char *)SW_BASE);
-            (*(char *)HEX3_HEX0_BASE) = hex_code[(int)switchValue];
+            int switchValue = (*(int *)SW_BASE);
+            (*(int *)HEX3_HEX0_BASE) = hex_code[(int)switchValue];
             offOrOn = 0;
         }
 
         else
         {
+            (*(int *)HEX3_HEX0_BASE) = 0;
             offOrOn = 1;
         }
 
